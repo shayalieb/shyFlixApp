12b99d3c4f270c9aae3ecfc383be9f8b31ba0653
shayalieb <105383935+shayalieb@users.noreply.github.com>
2022-10-28 11:23:19 -0400

    Added the original remote string
    mongodb+srv://shayalieberman:shaya1234@shyflixdb.hhh4rbo.mongodb.net/shyflixdb?retryWrites=true&w=majority

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ const
     const mongoose = require('mongoose');
     const Models = require('./models.js');
     
+    mongoose.connect('mongodb+srv://shayalieberman:shaya1234@shyflixdb.hhh4rbo.mongodb.net/shyflixdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });    
     //mongoose.connect('mongodb://localhost:27017/myapp')
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 let allowedOrgigins = ['http://localhost:8080', 'https://shyflixapp.herokuapp.com/'];
     
     app.use(bodyParser.json());
