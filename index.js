9f5e8681be31a6e5c5a6578e07642644ca629e3f
shayalieb <105383935+shayalieb@users.noreply.github.com>
2022-10-31 12:44:55 -0400

    updated with CONNECTION_URI

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const
     const mongoose = require('mongoose');
     const Models = require('./models.js');
     
-    mongoose.connect('mongodb+srv://shayalieberman:shaya1234@shyflixdb.hhh4rbo.mongodb.net/shyflixdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });    
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifeidTopology: true});    
+    // mongoose.connect('mongodb+srv://shayalieberman:shaya1234@shyflixdb.hhh4rbo.mongodb.net/shyflixdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });    
     //mongoose.connect('mongodb://localhost:27017/myapp')
 // mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 let allowedOrgigins = ['http://localhost:8080', 'https://shyflixapp.herokuapp.com/'];
