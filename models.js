d05f67f6b4871731919734ed8758a8ff551983cf
shayalieb <105383935+shayalieb@users.noreply.github.com>
2022-11-01 13:58:10 -0400

    Update models.js

--- models.js
+++ models.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 //importing the encryption function
 const bcrypt = require('bcrypt');
 
+const mongosh = require('mongosh')
+
 //Configring the movies database schema
 //Data tpyes are: String and Boolean
 let movieSchema = mongoose.Schema({
@@ -34,8 +36,8 @@ userSchema.statics.hashPassword = (password) => {
     return bcrypt.hashSync(password, 10);
 };
 
-userSchema.methods.validatePassword = function (Password) {
-    return bcrypt.compareSync(Password, this.password);
+userSchema.methods.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
 };
 
 //Creating the models 
