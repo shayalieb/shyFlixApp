38c34dffea404bda9d3250198e88e178fec7c154
shayalieb <105383935+shayalieb@users.noreply.github.com>
2022-11-02 12:13:39 -0400

    Tried rewriting the code for post login
    Would like to see if this works

--- auth.js
+++ auth.js
@@ -1,9 +1,10 @@
 const jwtSecret = 'your_jwt_secret';
 
+const { Router } = require('express');
 const jwt = require('jsonwebtoken');
 
 const passport = require('passport');
-require('./passport');
+  require('./passport');
 
 let generateJWTToken = (user) => {
     return jwt.sign(user, jwtSecret, {
@@ -12,25 +13,46 @@ let generateJWTToken = (user) => {
         algorithm: 'HS256'
     });
 }
-    
+ 
+//Post login
 module.exports = (router) => {
   router.post('/login', (req, res) => {
-    console.log('error', req.body);
-    passport.authenticate('local', { session: false }, (error, user, info) => {
+    passport.authenticate('local', {session: false}, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
-          message: 'Something is not right',
+          message: 'Some is not right over here',
           user: user
         });
       }
-      req.login(user, { session: false }, (error) => {
-        if (error) {
+      req.login(user, {session: false}, (error) => {
+        if(error) {
           res.send(error);
         }
         let token = generateJWTToken(user.toJSON());
-        return res.json({ user, token });
+        return res.json({user, token});
       });
-    })(req, res);
+    })(req, res)
   });
 }
+
+// module.exports = (router) => {
+//   router.post('/login', (req, res) => {
+//     console.log('error', req.body);
+//     passport.authenticate('local', { session: false }, (error, user, info) => {
+//       if (error || !user) {
+//         return res.status(400).json({
+//           message: 'Something is not right',
+//           user: user
+//         });
+//       }
+//       req.login(user, { session: false }, (error) => {
+//         if (error) {
+//           res.send(error);
+//         }
+//         let token = generateJWTToken(user.toJSON());
+//         return res.json({ user, token });
+//       });
+//     })(req, res);
+//   });
+// }
 
