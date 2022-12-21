ab707da4d11ae4473c0d36749e6a9b3ab1dded02
shayalieb <105383935+shayalieb@users.noreply.github.com>
2022-12-21 13:38:55 -0500

    Further updates on the database connection

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const { check, validationResult } = require('express-validator');
 app.use(express.json());
 
 //mongoose.set('strictQuery', true);
-mongoose.connect('mongodb+srv://shayalieberman:shaya1234@shyflixdb.hhh4rbo.mongodb.net/?retryWrites=true&w=majority').then(() => console.log('Connected!'));
-//mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb+srv://shayalieberman:shaya1234@shyflixdb.hhh4rbo.mongodb.net/?retryWrites=true&w=majority').then(() => console.log('Connected!'));
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const cors = require('cors');
 app.use(cors());
@@ -280,7 +280,10 @@ app.use((err, req, res, next) => {
 });
 
 //LISTENERS
-const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0', () => {
-    console.log('Listening on port ' + port)
-})
+const PORT = process.env.PORT || 8080;
+mongoose.connection.once('open', () => {
+    console.log('Connected to the database');
+    app.listen(PORT, '0.0.0.0', () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
