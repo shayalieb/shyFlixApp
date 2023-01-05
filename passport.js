fbf9f6e91d7e57d453a695175820a114ab1f9e96
shayalieb <105383935+shayalieb@users.noreply.github.com>
2023-01-05 14:42:11 -0500

    Removed secretOrKey

--- passport.js
+++ passport.js
@@ -29,7 +29,7 @@ passport.use(new LocalStrategy({
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt-secret'
+  // secretOrKey: 'your_jwt-secret'
 }, (jwtPayload, callback) => {
   return Users.findById(jwtPayload._id)
     .then((user) => {
