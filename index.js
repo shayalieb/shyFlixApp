47465017343c5d959c7b4f99ed7f802a35bf1254
shayalieb <105383935+shayalieb@users.noreply.github.com>
2023-01-06 12:54:47 -0500

    Added cors back as it did not work

--- index.js
+++ index.js
@@ -45,16 +45,16 @@ app.use(cors());
 
 
 
-// app.use(cors({
-//     origin: (origin, callback) => {
-//         if (!origin) return callback(null, true);
-//         if (allowedOrigins.indexOf(origin) === -1) {
-//             let message = 'Due to CORS policy for shyFlixApp access from origin is not allowed! ' + origin;
-//             return callback(new Error(message), false);
-//         }
-//         return callback(null, true);
-//     }
-// }));
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            let message = 'Due to CORS policy for shyFlixApp access from origin is not allowed! ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 
 let auth = require('./auth')(app);
