b7e013ad7829426b4e3f0f251bf8d29e0908b8a7
shayalieb <105383935+shayalieb@users.noreply.github.com>
2023-01-06 12:08:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ let allowedOrigins = [
     'https://shyflixapp.herokuapp.com/',
     'http://localhost:1234',
 ];
-//mongoose.connect('mongodb+srv://shayalieberman:shaya1234@shyflixdb.hhh4rbo.mongodb.net/shyflixdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb+srv://shayalieberman:shaya1234@shyflixdb.hhh4rbo.mongodb.net/shyflixdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 //mongoose.connect('mongodb://localhost:27017/myapp')
 //mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
