e750c310f1a453db77ddc03bf676d9bed04a80cf
shayalieb <105383935+shayalieb@users.noreply.github.com>
2023-01-11 14:14:08 -0500

    added mongoose.set("strictQuery", true);

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ const movies = models.movie;
 const users = models.user;
 //const mongoose = require('mongoose');
 
+mongoose.set("strictQuery", true);
+
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 let allowedOrigins = ['http://localhost:8080', 'https://shyflixapp.herokuapp.com/', 'http://localhost:1234'];
 //mongoose.connect('mongodb+srv://shayalieberman:shaya1234@shyflixdb.hhh4rbo.mongodb.net/shyflixdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });    
